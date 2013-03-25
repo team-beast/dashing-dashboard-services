e566cc5a5442eda71cba9d00d0344636657b1f49
Ben Flowers <ben.j.flowers@gmail.com>
2013-03-25 17:19:16 +0000

    adding gemlock

--- simple_service.rb
+++ simple_service.rb
@@ -30,7 +30,7 @@ end
 
 class DashboardNotifier
 	def push(list)
-		HTTParty.post('http://localhost:3030/widgets/broken_builds_push',
+		HTTParty.post('http://teambeast.herokuapp.com/widgets/broken_builds_push',
   		:body =>  { auth_token: "TEAM_BEAST_AUTH_TOKEN",items:list}.to_json)
 	end
 end
