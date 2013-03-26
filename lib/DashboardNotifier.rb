c9da6b9037454921492828a5e88c636111ee3382
Ben Flowers <ben.j.flowers@gmail.com>
2013-03-26 11:47:00 +0000

    adding correct auth token for dashboard

--- lib/DashboardNotifier.rb
+++ lib/DashboardNotifier.rb
@@ -1,6 +1,6 @@
 class DashboardNotifier
 	def push(list)
-		HTTParty.post('http://teambeast.herokuapp.com/widgets/broken_builds_push',
-  		:body =>  { auth_token: "TEAM_BEAST_AUTH_TOKEN",items:list}.to_json)
+		HTTParty.post('http://http://teambeast.herokuapp.com/widgets/broken_builds_push',
+  		:body =>  { auth_token: "YOUR_AUTH_TOKEN",items:list}.to_json)
 	end
 end
