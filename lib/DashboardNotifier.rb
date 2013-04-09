1c7d15125519830f558cf0a444fb6746a9dd5633
Ben Flowers <ben.j.flowers@gmail.com>
2013-04-09 15:07:13 +0100

    'dashboard_commit'

--- lib/DashboardNotifier.rb
+++ lib/DashboardNotifier.rb
@@ -3,7 +3,7 @@ class DashboardNotifier
 		@widget_id = widget_id
 	end
 	def push(list)
-		HTTParty.post('http://teambeast.herokuapp.com/widgets/#{widget_id}',
+		HTTParty.post("http://teambeast.herokuapp.com/widgets/#{@widget_id}",
   		:body =>  { auth_token: "YOUR_AUTH_TOKEN",items:list}.to_json)
 	end
 end
