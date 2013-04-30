d098678bf9730c298529340140bb581addbdfb4d
Iain Mitchell <iainjmitchell@gmail.com>
2013-04-30 14:21:33 +0100

    'dashboard_commit'

--- controllers/counter.rb
+++ controllers/counter.rb
@@ -1,3 +1,11 @@
+require_relative "../lib/DashboardNotifier"
+points = []
+
 get %r{/counter/([\w]+)} do | counter_name |
-	counter_name
+	build_status_notifier = DashboardNotifier.new(counter_name)
+	random_number = rand(30)
+	point = {:x => points.length, :y => random_number}
+	points.push(point)	
+	build_status_notifier.push({:points => points})
+	# counter_name
 end
